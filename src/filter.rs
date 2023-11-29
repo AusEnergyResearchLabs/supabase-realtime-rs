b047020cdaaca4318821a95a92ec4e13e57122b8
Liam Kinne <liam@kinne.io>
2023-11-29 10:13:16 +1000

    refactor: supabase only supports single filters

--- src/filter.rs
+++ src/filter.rs
@@ -1,93 +1,72 @@
 //! Postgres change filtering.
 
-struct Filter {
-    queries: Vec<(String, String)>,
-}
+/// Associated filter builders.
+///
+///
+/// # Example
+///
+/// ```rust
+/// let filter = Filter::eq("column_name", "value");
+/// ```
+struct Filter {}
 
 impl Filter {
-    /// Creates a new `Filter`.
-    ///
-    ///
-    /// # Example
-    ///
-    /// ```rust
-    /// let filter = Filter::new()
-    ///     .eq("column_name", "foo");
-    /// ```
-    pub fn new() -> Self {
-        Self {
-            queries: Vec::new(),
-        }
-    }
-
     /// Filter by rows whose value on the stated `column` exactly matches the specified `filter`.
-    pub fn eq<T, U>(mut self, column: T, filter: U) -> Self
+    pub fn eq<T, U>(column: T, value: U) -> String
     where
         T: AsRef<str>,
         U: AsRef<str>,
     {
-        self.queries
-            .push((column.as_ref().into(), format!("eq.{}", filter.as_ref())));
-        self
+        format!("{}=eq.{}", column.as_ref(), value.as_ref())
     }
 
     /// Filter by rows whos value on the stated `column` doesn't match the specified `filter`.
-    pub fn neq<T, U>(mut self, column: T, filter: U) -> Self
+    pub fn neq<T, U>(mut self, column: T, value: U) -> String
     where
         T: AsRef<str>,
         U: AsRef<str>,
     {
-        self.queries
-            .push((column.as_ref().into(), format!("neq.{}", filter.as_ref())));
-        self
+        format!("{}=neq.{}", column.as_ref(), value.as_ref())
     }
 
     /// Filter by rows whose value on the stated `column` is greater than the specified `filter`.
-    pub fn gt<T, U>(mut self, column: T, filter: U) -> Self
+    pub fn gt<T, U>(mut self, column: T, value: U) -> String
     where
         T: AsRef<str>,
         U: AsRef<str>,
     {
-        self.queries
-            .push((column.as_ref().into(), format!("gt.{}", filter.as_ref())));
-        self
+        format!("{}=gt.{}", column.as_ref(), value.as_ref())
     }
 
     /// Filter by rows whose value on the stated `column` is greater than or equal to the specified `filter`.
-    pub fn gte<T, U>(mut self, column: T, filter: U) -> Self
+    pub fn gte<T, U>(mut self, column: T, value: U) -> String
     where
         T: AsRef<str>,
         U: AsRef<str>,
     {
-        self.queries
-            .push((column.as_ref().into(), format!("gte.{}", filter.as_ref())));
-        self
+        format!("{}=gte.{}", column.as_ref(), value.as_ref())
     }
 
     /// Filter by rows whose value on the stated `column` is less than the specified `filter`.
-    pub fn lt<T, U>(mut self, column: T, filter: U) -> Self
+    pub fn lt<T, U>(mut self, column: T, value: U) -> String
     where
         T: AsRef<str>,
         U: AsRef<str>,
     {
-        self.queries
-            .push((column.as_ref().into(), format!("lt.{}", filter.as_ref())));
-        self
+        format!("{}=lt.{}", column.as_ref(), value.as_ref())
     }
 
     /// Filter by rows whose value on the stated `column` is less than or equal to the specified `filter`.
-    pub fn lte<T, U>(mut self, column: T, filter: U) -> Self
+    pub fn lte<T, U>(mut self, column: T, value: U) -> String
     where
         T: AsRef<str>,
         U: AsRef<str>,
     {
-        self.queries
-            .push((column.as_ref().into(), format!("lte.{}", filter.as_ref())));
-        self
+        format!("{}=lte.{}", column.as_ref(), value.as_ref())
     }
 
     /// Filter by rows whose value on the stated `column` is found on the specified `values`.
-    pub fn in_<T, U, V>(mut self, column: T, values: U) -> Self
+    pub fn in_<T, U, V>(mut self, column: T, values: U) -> String
     where
         T: AsRef<str>,
         U: IntoIterator<Item = V>,
@@ -96,21 +75,9 @@ impl Filter {
         let mut values: String = values
             .into_iter()
             .fold(String::new(), |a, s| a + s.as_ref() + ",");
-        values.pop();
-        self.queries
-            .push((column.as_ref().into(), format!("in.({})", values)));
-        self
-    }
+        values.pop(); // remove last oomma
 
-    /// Build the filter statement.
-    pub fn build(self) -> String {
-        let f: String = self
-            .queries
-            .into_iter()
-            .map(|f| format!("{}={}", f.0, f.1))
-            .collect::<Vec<String>>()
-            .join(",");
-        f
+        format!("{}=in.({})", column.as_ref(), values)
     }
 }
 
@@ -119,28 +86,9 @@ mod tests {
     use super::*;
 
     #[test]
-    fn no_filters() {
-        let f = Filter::new();
-        let built = f.build();
-
-        assert_eq!(built, "");
-    }
-
-    #[test]
     fn column_equals() {
-        let f = Filter::new().eq("foo_column", "bar_value");
-        let built = f.build();
+        let f = Filter::eq("foo_column", "bar_value");
 
-        assert_eq!(built, "foo_column=eq.bar_value");
-    }
-
-    #[test]
-    fn column_chain_equals() {
-        let f = Filter::new()
-            .eq("foo_column", "bar_value")
-            .eq("foo_column", "baz_value");
-        let built = f.build();
-
-        assert_eq!(built, "foo_column=eq.bar_value,foo_column=eq.baz_value");
+        assert_eq!(f, "foo_column=eq.bar_value");
     }
 }
