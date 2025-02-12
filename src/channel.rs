99ccbf583d539f40bc9b79fc6260dc5020390fdc
Liam Kinne <liam@kinne.io>
2025-02-12 11:18:43 +1000

    add basic broadcast channel implementation

---
+++ src/channel.rs
@@ -0,0 +1,141 @@
+use crate::{
+    protocol::{
+        BroadcastMessage, HeartbeatMessage, LeaveMessage, PhoenixMessage, PostgresChangesMessage,
+        PresenceDiffMessage, PresenceStateMessage, Topic,
+    },
+    Error,
+};
+use std::{collections::HashMap, marker::PhantomData, task::Poll, time::Duration};
+use tokio::{
+    sync::mpsc::{Receiver, Sender},
+    task::{self, AbortHandle},
+};
+
+#[derive(Debug)]
+pub struct Broadcast;
+
+#[derive(Debug)]
+pub struct Presence;
+
+#[derive(Debug)]
+pub struct Postgres;
+
+/// Subscription instance.
+pub struct Subscription<T> {
+    pub(crate) _t: PhantomData<T>,
+    pub(crate) topic: Topic,
+    pub(crate) receiver: Receiver<PhoenixMessage>,
+    pub(crate) sender: Sender<PhoenixMessage>,
+    pub(crate) heartbeat: AbortHandle,
+}
+
+impl<T> Subscription<T> {
+    pub fn new(
+        topic: Topic,
+        receiver: Receiver<PhoenixMessage>,
+        sender: Sender<PhoenixMessage>,
+    ) -> Self {
+        // spawn heartbeat task. cleaned up on drop.
+        let heartbeat_sender = sender.clone();
+        let heartbeat_topic = topic.clone();
+        let heartbeat = task::spawn(async move {
+            loop {
+                tokio::time::sleep(Duration::from_secs(25)).await;
+                if let Err(e) = heartbeat_sender
+                    .send(PhoenixMessage::Heartbeat(HeartbeatMessage {
+                        topic: heartbeat_topic.clone(),
+                        payload: HashMap::new(),
+                        reference: "".to_owned(), // todo: use an actual reference
+                    }))
+                    .await
+                {
+                    tracing::error!("Send heartbeat failed: {}", e);
+                }
+            }
+        })
+        .abort_handle();
+
+        Self {
+            _t: PhantomData::default(),
+            topic,
+            sender,
+            receiver,
+            heartbeat,
+        }
+    }
+}
+
+impl<T> Drop for Subscription<T> {
+    fn drop(&mut self) {
+        self.heartbeat.abort();
+    }
+}
+
+impl futures::Stream for Subscription<Broadcast> {
+    type Item = BroadcastMessage;
+
+    fn poll_next(
+        mut self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Option<Self::Item>> {
+        match self.receiver.poll_recv(cx) {
+            Poll::Ready(msg) => match msg {
+                Some(PhoenixMessage::Broadcast(bcast)) => Poll::Ready(Some(bcast)),
+                Some(_) => {
+                    cx.waker().wake_by_ref();
+                    Poll::Pending
+                }
+                None => Poll::Ready(None),
+            },
+            Poll::Pending => {
+                cx.waker().wake_by_ref();
+                Poll::Pending
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum PresenceMessage {
+    State(PresenceStateMessage),
+    Diff(PresenceDiffMessage),
+}
+
+impl futures::Stream for Subscription<Presence> {
+    type Item = PresenceMessage;
+
+    fn poll_next(
+        mut self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> Poll<Option<Self::Item>> {
+        match self.receiver.poll_recv(cx) {
+            Poll::Ready(msg) => match msg {
+                Some(PhoenixMessage::PresenceState(state)) => {
+                    Poll::Ready(Some(PresenceMessage::State(state)))
+                }
+                Some(PhoenixMessage::PresenceDiff(diff)) => {
+                    Poll::Ready(Some(PresenceMessage::Diff(diff)))
+                }
+                _ => Poll::Pending,
+            },
+            Poll::Pending => Poll::Pending,
+        }
+    }
+}
+
+impl futures::Stream for Subscription<Postgres> {
+    type Item = PostgresChangesMessage;
+
+    fn poll_next(
+        mut self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Option<Self::Item>> {
+        match self.receiver.poll_recv(cx) {
+            Poll::Ready(msg) => match msg {
+                Some(PhoenixMessage::PostgresChanges(changes)) => Poll::Ready(Some(changes)),
+                _ => Poll::Pending,
+            },
+            Poll::Pending => Poll::Pending,
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,188 @@
+mod channel;
+mod protocol;
 
+pub use protocol::{
+    BroadcastConfig, BroadcastPayload, PostgresConfig, PostgresEvent, PresenceConfig,
+};
+
+use channel::{Broadcast, Subscription};
+use futures::{SinkExt, StreamExt};
+use protocol::{
+    AccessTokenMessage, AccessTokenPayload, BroadcastMessage, Config, JoinMessage, JoinPayload,
+    PhoenixMessage, Status, Topic,
+};
+use std::sync::{
+    atomic::{AtomicU32, Ordering},
+    Arc,
+};
+use tokio::{
+    sync::{mpsc, Mutex},
+    task,
+};
+use tokio_tungstenite::{connect_async, tungstenite::Message};
+
+type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
+
+#[derive(Debug)]
+pub struct Client {
+    sender: mpsc::Sender<PhoenixMessage>,
+    receivers: Arc<Mutex<Vec<(String, mpsc::Sender<PhoenixMessage>)>>>,
+    reference: Arc<AtomicU32>,
+}
+
+impl Client {
+    /// Creates a new realtime client.
+    pub async fn connect(
+        endpoint: impl Into<String>,
+        token: impl Into<String>,
+    ) -> Result<Self, Error> {
+        let url = format!("{}?apikey={}", endpoint.into(), token.into());
+        let (stream, _) = connect_async(&url).await?;
+        let (mut write, mut read) = stream.split();
+
+        let receivers: Arc<Mutex<Vec<(String, mpsc::Sender<PhoenixMessage>)>>> =
+            Arc::new(Mutex::new(Vec::new()));
+        let incomming_receivers = receivers.clone();
+
+        // incomming event listener
+        task::spawn(async move {
+            while let Some(message) = read.next().await {
+                let message = match message {
+                    Ok(Message::Text(text)) => {
+                        match serde_json::from_str::<PhoenixMessage>(&text) {
+                            Ok(message) => {
+                                tracing::debug!("recv: {:?}", message);
+                                message
+                            }
+                            Err(e) => {
+                                tracing::error!("failed to parse: {}", e);
+                                continue;
+                            }
+                        }
+                    }
+                    Ok(_) => continue,
+                    Err(e) => {
+                        tracing::error!("{}", e);
+                        continue;
+                    }
+                };
+
+                incomming_receivers.lock().await.retain(|(topic, sender)| {
+                    if topic != message.topic().as_str() {
+                        true
+                    } else {
+                        match sender.try_send(message.clone()) {
+                            Ok(_) => true,
+                            Err(mpsc::error::TrySendError::Full(_)) => true,
+                            Err(mpsc::error::TrySendError::Closed(_)) => false,
+                        }
+                    }
+                });
+            }
+        });
+
+        // outgoing queue
+        let (sender, mut outgoing_receiver) = mpsc::channel(128);
+        task::spawn(async move {
+            loop {
+                if let Some(message) = outgoing_receiver.recv().await {
+                    tracing::debug!("send: {:?}", message);
+                    match serde_json::to_string(&message) {
+                        Ok(text) => {
+                            if let Err(e) = write.send(Message::Text(text)).await {
+                                tracing::error!("{}", e);
+                            }
+                        }
+                        Err(e) => tracing::error!("Failed to serialize message: {}", e),
+                    }
+                }
+            }
+        });
+
+        Ok(Client {
+            sender,
+            receivers,
+            reference: Arc::new(AtomicU32::new(1)),
+        })
+    }
+
+    /// Update the acess token.
+    pub async fn access_token(&mut self, token: impl Into<String>) -> Result<(), Error> {
+        let reference = self.reference.fetch_add(1, Ordering::Relaxed).to_string();
+
+        self.sender
+            .send(PhoenixMessage::AccessToken(AccessTokenMessage {
+                topic: Topic::new(""),
+                payload: AccessTokenPayload {
+                    access_token: token.into(),
+                },
+                reference,
+            }))
+            .await?;
+
+        Ok(())
+    }
+
+    pub async fn broadcast(
+        &self,
+        topic: impl Into<String>,
+        payload: BroadcastPayload,
+    ) -> Result<(), Error> {
+        self.sender
+            .send(PhoenixMessage::Broadcast(BroadcastMessage {
+                topic: Topic::from(topic.into()),
+                payload,
+                reference: Some(self.reference.fetch_add(1, Ordering::Relaxed).to_string()),
+            }))
+            .await?;
+
+        Ok(())
+    }
+
+    /// Listen to broadcast messages.
+    pub async fn on_broadcast(
+        &mut self,
+        topic: impl Into<String>,
+        config: BroadcastConfig,
+    ) -> Result<Subscription<Broadcast>, Error> {
+        let topic = topic.into();
+
+        let (sender, mut receiver) = mpsc::channel(128);
+        self.receivers.lock().await.push((topic.to_owned(), sender));
+
+        let reference = self.reference.fetch_add(1, Ordering::Relaxed).to_string();
+        self.sender
+            .send(PhoenixMessage::Join(JoinMessage {
+                topic: topic.clone().into(),
+                payload: JoinPayload {
+                    config: Config::broadcast(config),
+                },
+                reference: reference.clone(),
+            }))
+            .await?;
+
+        loop {
+            let response = receiver.recv().await;
+
+            match response {
+                Some(PhoenixMessage::Reply(reply)) => {
+                    if reply.reference == reference {
+                        if reply.payload.status == Status::Ok {
+                            return Ok(Subscription::<Broadcast>::new(
+                                topic.into(),
+                                receiver,
+                                self.sender.clone(),
+                            ));
+                        } else {
+                            return Err(Error::from("failed to join channel"));
+                        }
+                    }
+                }
+                Some(_) => {}
+                None => {
+                    return Err(Error::from("stream closed"));
+                }
+            }
+        }
+    }
+}
